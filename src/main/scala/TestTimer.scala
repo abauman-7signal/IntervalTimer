de710827bcc6db8a9111c8a5171d2d0399dfd3fc
Aaron Bauman <aaron.bauman@7signal.com>
2021-01-07 09:36:00 -0500

    Tracks the times the interval has been crossed

--- src/main/scala/TestTimer.scala
+++ src/main/scala/TestTimer.scala
@@ -3,15 +3,29 @@ import java.lang.System.currentTimeMillis
 class TestTimer(val intervalInMs: Long) {
   val IntervalInMs: Long = intervalInMs
   val StartTimeInMs: Long = currentTimeMillis()
-  var lastIntervalCrossed: Long = 0
+  private var lastIntervalCrossed: Long = 0
+  private var numberTimesCrossed: Long = 0
 
-  def hasCrossedInterval: Boolean = {
+  private def hasCrossedInterval: Boolean = {
     val currentInterval = (currentTimeMillis() - StartTimeInMs) / IntervalInMs
 
     if (currentInterval > lastIntervalCrossed) {
+      println(s"numberTimesCrossed: $numberTimesCrossed")
+      numberTimesCrossed = numberTimesCrossed + currentInterval - lastIntervalCrossed
       lastIntervalCrossed = currentInterval
       return true
     }
-    return false
+    false
+  }
+
+  def getNumberTimesCrossed: Long = {
+    hasCrossedInterval
+    numberTimesCrossed
+  }
+
+  def getNumberTimesCrossedAndClear: Long = {
+    val numTimesCrossed = getNumberTimesCrossed
+    numberTimesCrossed = 0
+    numTimesCrossed
   }
 }
--- src/test/scala/TestTimerTest.scala
+++ src/test/scala/TestTimerTest.scala
@@ -2,39 +2,57 @@ import org.scalatest.FunSuite
 
 class TestTimerTest extends FunSuite {
   test("when instantiated, object set to known state") {
-    val subject = new TestTimer(1)
-    assert(subject.IntervalInMs == 1)
+    val subject = new TestTimer(100)
+    assert(subject.IntervalInMs == 100)
     assert(subject.StartTimeInMs > 0)
-    assert(subject.lastIntervalCrossed == 0)
+//    assert(subject.lastIntervalCrossed == 0)
+    assert(subject.getNumberTimesCrossed == 0)
   }
 
   test("when has not crossed interval") {
     val subject = new TestTimer(100)
-    assert(!subject.hasCrossedInterval)
+//    assert(!subject.hasCrossedInterval)
+    assert(subject.getNumberTimesCrossed == 0)
   }
 
   test("when has crossed interval") {
-    val subject = new TestTimer(1)
-    Thread.sleep(10)
-    assert(subject.hasCrossedInterval)
+    val subject = new TestTimer(50)
+    Thread.sleep(50)
+//    assert(subject.hasCrossedInterval)
+    assert(subject.getNumberTimesCrossed == 1)
   }
 
   test("when has crossed second interval") {
     val subject = new TestTimer(50)
-    assert(!subject.hasCrossedInterval)
+//    assert(!subject.hasCrossedInterval)
     Thread.sleep(50)
-    assert(subject.hasCrossedInterval)
-    assert(!subject.hasCrossedInterval)
+//    assert(subject.hasCrossedInterval)
+//    assert(!subject.hasCrossedInterval)
+    assert(subject.getNumberTimesCrossed == 1)
     Thread.sleep(50)
-    assert(subject.hasCrossedInterval)
-    assert(!subject.hasCrossedInterval)
+//    assert(subject.hasCrossedInterval)
+//    assert(!subject.hasCrossedInterval)
+    assert(subject.getNumberTimesCrossed == 2)
   }
 
   test("when has crossed 2 intervals without being checked") {
     val subject = new TestTimer(30)
-    assert(!subject.hasCrossedInterval)
+//    assert(!subject.hasCrossedInterval)
+    assert(subject.getNumberTimesCrossed == 0)
     Thread.sleep(70)
-    assert(subject.hasCrossedInterval)
-    assert(!subject.hasCrossedInterval)
+//    assert(subject.hasCrossedInterval)
+//    assert(!subject.hasCrossedInterval)
+    assert(subject.getNumberTimesCrossed == 2)
+  }
+
+  test("when has crossed interval and accessed times crossed interval with clear") {
+    val subject = new TestTimer(50)
+    Thread.sleep(50)
+//    assert(subject.hasCrossedInterval)
+    assert(subject.getNumberTimesCrossed == 1)
+    Thread.sleep(50)
+    assert(subject.getNumberTimesCrossed == 2)
+    assert(subject.getNumberTimesCrossedAndClear == 2)
+    assert(subject.getNumberTimesCrossed == 0)
   }
 }
